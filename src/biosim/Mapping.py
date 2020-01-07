26a715178890dae03320969fb80a3747cfa8d3de
Anders Huse <huse.anders@gmail.com>
2020-01-07 13:52:20 +0100

    maade mapping.py and wrote some code

---
+++ src/biosim/Mapping.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Geo class
+"""
+
+__author__ = "Anders Huse, Bishnu Poudel"
+__email__ = "anhuse@nmbu.no; bipo@nmbu.no"
+
+import ...
+
+
+class Map:
+    def __init__(self, row, column):
+        self.row = row
+        self.column = column
+
+
+
+
