9801428dd8dc621d8964257c4548dab4effdb1ad
Anders Huse <huse.anders@gmail.com>
2020-01-07 13:52:52 +0100

    added several classes

--- src/biosim/Mapping.py
+++ src/biosim/Mapping.py
@@ -16,5 +16,57 @@ class Map:
         self.column = column
 
 
+class Sav(Map):
+    f_max = 300
+
+    def __init__(self, f_ij=300, alpha=0.3, row, column, num_carn,
+                 num_herb):
+        """
+        parameters
+        """
+        self.food = f_ij
+        self.aplha = alpha
+        self.num_carn = num_carn
+        self.num_herb = num_herb
+
+        super().__init__()
+
+
+class Jungle(Map):
+    f_max = 800
+
+    def __int__(self, f_ij=f_max, row, column, num_carn, num_herb):
+        """
+        Parameters
+        """
+        self.food = f_ij
+        self.num_carn = num_carn
+        self.num_herb = num_herb
+
+        super().__init__()
+
+
+class Desert(Map):
+    f_max = 0
+
+    def __init__(self, f_ij=f_max, row, column, num_carn, num_herb):
+        """
+        Parameters
+        """
+        self.food = f_ij
+        self.num_herb = num_herb
+        self.carn = num_carn
+
+        super().__init__()
+
+
+class Ocean(Map):
+    def __init__(self, row, column):
+        super().__init__()
+
+
+class Mountian(Map):
+    def __init__(self, row, column):
+        super().__init__()
 
 
