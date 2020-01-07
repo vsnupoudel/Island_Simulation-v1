b97cc5ba6269d38e41869d3e5a9105601bd50fe7
Anders Huse <huse.anders@gmail.com>
2020-01-07 10:32:53 +0100

    small changes

--- src/biosim/Geography.py
+++ src/biosim/Geography.py
@@ -19,16 +19,37 @@ class Geo:
     def __init__(self, geo_matrix_input_string):
         self.geo_graph = textwrap.dedent(geo_matrix_input_string)
 
-    def geo_graph(self):
+    def geo_2D(self):
         """
         Makes a 2D list of the coordinates for the geography
         """
-        lines = self.geo_graph_string.splitline()
+        lines = self.geo_graph.splitline()
         geo_list = [ list(_) for _ in lines]
         return geo_list
 
     @property
     def geo_shape(self):
         return np.shape(self.geo_graph)
+
+
+if __name__ == "__main__":
+    g = Geo("""\
+    OOOOOOOOOOOOOOOOOOOOO
+    OOOOOOOOSMMMMJJJJJJJO
+    OSSSSSJJJJMMJJJJJJJOO
+    OSSSSSSSSSMMJJJJJJOOO
+    OSSSSSJJJJJJJJJJJJOOO
+    OSSSSSJJJDDJJJSJJJOOO
+    OSSJJJJJDDDJJJSSSSOOO
+    OOSSSSJJJDDJJJSOOOOOO
+    OSSSJJJJJDDJJJJJJJOOO
+    OSSSSJJJJDDJJJJOOOOOO
+    OOSSSSJJJJJJJJOOOOOOO
+    OOOSSSSJJJJJJJOOOOOOO
+    OOOOOOOOOOOOOOOOOOOOO""")
+
+    a = g.geo_2D()
+    print(a)
+
 
 
