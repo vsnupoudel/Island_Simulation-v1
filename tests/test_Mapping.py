2738788db44a0bdb0000a331446472f7c410dc32
Anders Huse <huse.anders@gmail.com>
2020-01-07 14:09:02 +0100

    made test_mapping.py and made some tests

---
+++ tests/test_Mapping.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+"""
+Geo class
+"""
+
+__author__ = "Anders Huse, Bishnu Poudel"
+__email__ = "anhuse@nmbu.no; bipo@nmbu.no"
+
+
+def test_positive_input():
+    """all input sholud be positive integers"""
+    pass
+
+def test_positive_num_carn():
+    """number of carnevoirs is a positive integer"""
+    pass
+
+def test_positive_num_herb():
+    """number of herbevoirs is a positive integer"""
+    pass
+
