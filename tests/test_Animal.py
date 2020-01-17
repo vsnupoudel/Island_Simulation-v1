3415dae2a45fe29d40e37d5b9df8736ca7124ccb
Anders Huse <anhuse@nmbu.no>
2020-01-17 12:46:03 +0100

    small change

--- tests/test_Animal.py
+++ tests/test_Animal.py
@@ -31,6 +31,23 @@ def test_positive_fitness():
     assert h.fitness > 0
     assert c.fitness > 0
 
+def test_up_par():
+    """Herbivore and Carnivore parameters should be updated"""
+    h = Herbivore(2, 10)
+    c = Carnivore(2, 10)
+    h.up_par({"zeta": 3.2, "xi": 1.8})
+    c.up_par({"zeta": 5.0, "xi": 2.0})
+
+    assert Herbivore.p["zeta"] == 3.2
+    assert Herbivore.p["xi"] == 1.8
+    assert Carnivore.p["zeta"] == 5.0
+    assert Carnivore.p["xi"] == 2.0
+
+
+def test_herb_eat_weigth_increase():
+    """Herbevoirs weigth should increase when eating"""
+    pass
+
 
 
 
