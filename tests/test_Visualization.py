f9ab613ed790be7eada0693a5e0ed339e6f91f35
Anders Huse <anhuse@nmbu.no>
2020-01-18 14:53:28 +0100

    made class for all tests in test_Visualization

--- tests/test_Visualization.py
+++ tests/test_Visualization.py
@@ -14,74 +14,85 @@ from biosim.Animal import Herbivore, Carnivore
 from biosim.Visualization import Visualization
 
 import numpy as np
-
-map = """\
-             OOOOOOOOO
-             OSSJJSSOO
-             OOOOOOOOO
-             """
-
-ini_herbs = [
-    {
-        "loc": (2, 2),
-        "pop": [
-            {"species": "Herbivore", "age": 5, "weight": 20}
-            for _ in range(200)
-        ],
-    }
-]
-
-s = BioSim(map, ini_herbs, seed=1)
-v = Visualization(s.object_matrix)
+import pytest
 
 
-def test_set_graphics():
-    """tests subplots of set_graphics"""
-    v._set_graphics()
-    assert v._fig is not None
-    assert v._map_ax is not None
-    assert v._herb_line is not None
-    assert v._carn_line is not None
-    assert v._herb_ax is not None
-    assert v._carn_ax is not None
+class TestVisualization:
+    """
+    Several tests for the Visualization class
+    """
+    @pytest.fixture()
+    def create_sim(self):
+        """Creates a Simulation object"""
+        map = """\
+                     OOOOOOOOO
+                     OSSJJSSOO
+                     OOOOOOOOO
+                     """
+
+        ini_herbs = [
+            {
+                "loc": (2, 2),
+                "pop": [
+                    {"species": "Herbivore", "age": 5, "weight": 20}
+                    for _ in range(200)
+                ],
+            }
+        ]
+
+        return BioSim(map, ini_herbs, seed=1)
+
+    @pytest.fixture()
+    def create_vis(self, create_sim):
+        """Creates a Visualization object"""
+        return Visualization(create_sim.object_matrix)
+
+    def test_set_graphics(self, create_vis):
+        """tests subplots of set_graphics"""
+        create_vis._set_graphics(5)
+        assert create_vis._fig is not None
+        assert create_vis._map_ax is not None
+        assert create_vis._herb_line is not None
+        assert create_vis._carn_line is not None
+        assert create_vis._herb_ax is not None
+        assert create_vis._carn_ax is not None
+
+    def test_create_map(self, create_vis, create_sim):
+        """Map should be created"""
+        create_vis._set_graphics(5)
+        create_vis.create_map(create_sim.island_matrix)
+        assert create_vis._img_axis is not None
+
+    def test_update_herb_ax(self, create_vis, create_sim):
+        """map of herbivores should be created"""
+        create_vis._set_graphics(5)
+        ax_1 = create_vis._herb_axis
+        create_vis.update_herb_ax(create_sim.herbivore_distribution)
+        ax_2 = create_vis._herb_axis
+
+        assert create_vis._herb_axis is not None
+        assert ax_1 != ax_2
+
+    def test_update_carn_ax(self, create_vis, create_sim):
+        """map of carnivores should be created"""
+        create_vis._set_graphics(5)
+        ax_1 = create_vis._carn_axis
+        create_vis.update_carn_ax(create_sim.carnivore_distribution)
+        ax_2 = create_vis._carn_axis
+
+        assert create_vis._carn_axis is not None
+        assert ax_1 != ax_2
+
+    def test_update_mean_ax(self):
+        """herbivore and carnivore line should be created"""
+        pass
+
+    def test_make_movie(self):
+        """makes movie"""
+        pass
 
 
-def test_create_map():
-    """Map should be created"""
-    v._set_graphics()
-    v.create_map(s.island_matrix)
-    assert v._img_axis is not None
 
-
-def test_update_herb_ax():
-    """map of herbivores should be created"""
-    v._set_graphics()
-    ax_1 = v._herb_axis
-    v.update_herb_ax(s.herbivore_distribution)
-    ax_2 = v._herb_axis
-
-    assert v._herb_axis is not None
-    assert ax_1 != ax_2
-
-
-def test_update_carn_ax():
-    """map of carnivores should be created"""
-    v._set_graphics()
-    ax_1 = v._carn_axis
-    v.update_carn_ax(s.carnivore_distribution)
-    ax_2 = v._carn_axis
-
-    assert v._carn_axis is not None
-    assert ax_1 != ax_2
-
-
-def test_update_mean_ax():
-    """"""
-    pass
-
-
-def test_make_movie():
-    """makes movie"""
 
 
 
