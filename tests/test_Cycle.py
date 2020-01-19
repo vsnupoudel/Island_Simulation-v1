dd32bccf9b35f03c4d95fe3b1311fab83c27094c
vsnupoudel <replytobishnu@gmail.com>
2020-01-19 21:38:34 +0100

    Modified fixtures to make tests shorter

--- tests/test_Cycle.py
+++ tests/test_Cycle.py
@@ -8,137 +8,133 @@ __author__ = "Anders Huse, Bishnu Poudel"
 __email__ = "anhuse@nmbu.no; bipo@nmbu.no"
 
 from Cycle import Cycle
-from Geography import Geo
-# from Mapping import Cell, Savannah, Jungle
 from simulation import BioSim
-import pytest
 from pytest_mock import mocker
+import pytest
 import numpy
 
+@pytest.fixture()
+def input_map():
+    map = ("""\
+                    OOOO
+                    OJSO
+                    OOOO""")
+    return map
 
-input_map = ("""\
-                OOOO
-                OJSO
-                OOOO""")
-ini_herbs = [
-    {
-        "loc": (1, 1),
-        "pop": [
-            {"species": "Herbivore", "age": 5, "weight": 50}
-            for _ in range(120)
-        ]
-    }
-]+[
-    {
-        "loc": (1, 1),
-        "pop": [
-            {"species": "Carnivore", "age": 5, "weight": 50}
-            for _ in range(20)
-        ]
-    }
-]
+@pytest.fixture()
+def ini_herbs():
+    herbs = [
+                    {
+                        "loc": (1, 1),
+                        "pop": [
+                            {"species": "Herbivore", "age": 5, "weight": 50}
+                            for _ in range(120)
+                        ]
+                    }
+                ] + [
+                    {
+                        "loc": (1, 1),
+                        "pop": [
+                            {"species": "Carnivore", "age": 5, "weight": 50}
+                            for _ in range(20)
+                        ]
+                    }
+                ]
+    return herbs
 
-s = BioSim(input_map, ini_herbs, seed=1)
-c = Cycle(s.object_matrix)
+@pytest.fixture()
+def create_s(input_map,ini_herbs):
+    s = BioSim(input_map, ini_herbs, seed=1)
+    return s
 
-def test_get_adjacent_migratable_cells():
+@pytest.fixture()
+def create_c(input_map,ini_herbs, create_s):
+    c = Cycle(create_s.object_matrix)
+    return c
+
+def test_get_adjacent_migratable_cells(create_c):
     """should only get migratable cells"""
     migratable_cells  = [type(cell).__name__ for cell in
-                       c.get_adjacent_migratable_cells(1, 1)]
+                       create_c.get_adjacent_migratable_cells(1, 1)]
     assert migratable_cells == ['Savannah']
 
-def test_food_grows_Savannah():
+def test_food_grows_Savannah(create_c):
     """Food amount in each Savannah cell should increase"""
-    prev_food_sav = c.object_matrix[1][2].f_ij
-    c.food_grows()
-    assert c.object_matrix[1][2].f_ij > prev_food_sav
+    prev_food_sav = create_c.object_matrix[1][2].f_ij
+    create_c.food_grows()
+    assert create_c.object_matrix[1][2].f_ij > prev_food_sav
 
-def test_max_food_Jungle():
+def test_max_food_Jungle(create_c):
     """When food grows in Jungle cells it should be set to f_max = 800"""
 
-    prev_food_jun = c.object_matrix[1][1].f_ij
-    c.food_grows()
-    assert c.object_matrix[1][1].f_ij >= prev_food_jun
-    assert c.object_matrix[1][1].f_ij == c.object_matrix[1][1].parameters[
+    prev_food_jun = create_c.object_matrix[1][1].f_ij
+    create_c.food_grows()
+    assert create_c.object_matrix[1][1].f_ij >= prev_food_jun
+    assert create_c.object_matrix[1][1].f_ij == create_c.object_matrix[1][1].parameters[
         'f_max']
 
-def test_fitness_increase_after_feeding():
-    prev_fitness_array = [ a.fitness for a in c.object_matrix[1][
+def test_fitness_increase_after_feeding(create_c):
+    prev_fitness_array = [ a.fitness for a in create_c.object_matrix[1][
         1].animal_object_list ]
-    c.food_grows()
-    c.animals_eat()
-    curr_fitness_array = [a.fitness for a in c.object_matrix[1][
+    create_c.food_grows()
+    create_c.animals_eat()
+    curr_fitness_array = [a.fitness for a in create_c.object_matrix[1][
         1].animal_object_list]
     # cached property was not working in fitness, so removed it
     assert curr_fitness_array > prev_fitness_array
 
-@pytest.fixture()
-def h_count():
-    return len([a for a in s.object_matrix[1][1].animal_object_list
-    if type(a).__name__ == "Herbivore"] )
 
-@pytest.fixture()
-def c_count():
-    return len([a for a in s.object_matrix[1][1].animal_object_list
-                   if type(a).__name__ == "Carnivore"])
+def test_animals_reproduce(input_map, ini_herbs):
+    s = BioSim(input_map, ini_herbs, seed=1)
+    herb_count_prev = s.object_matrix[1][1].n_herbs
+    carn_count_prev = s.object_matrix[1][1].n_carns
 
-
-def test_animals_reproduce(h_count, c_count):
-    herb_count_prev = h_count
-    carn_count_prev = c_count
-
+    c = Cycle(s.object_matrix)
     c.food_grows()
     c.animals_reproduce()
-    herb_count_curr = len([a for a in s.object_matrix[1][1].animal_object_list
-    if type(a).__name__ == "Herbivore"])
-    carn_count_curr = len([a for a in s.object_matrix[1][1].animal_object_list
-                   if type(a).__name__ == "Carnivore"])
+    herb_count_curr = s.object_matrix[1][1].n_herbs
+    carn_count_curr = s.object_matrix[1][1].n_carns
 
     assert carn_count_curr > carn_count_prev
     assert herb_count_curr > herb_count_prev
 
-def test_animals_migrate(mocker):
+def test_animals_migrate(mocker, create_c, create_s):
     mocker.patch('numpy.random.random', return_value=0)
-    c.animals_migrate()
-    assert len(s.object_matrix[1][1].animal_object_list) == 0
-    assert len(s.object_matrix[1][2].animal_object_list) == 140
+    create_c.animals_migrate()
+    assert len(create_s.object_matrix[1][1].animal_object_list) == 0
+    assert len(create_s.object_matrix[1][2].animal_object_list) == 140
 
-def test_animals_dont_migrate(mocker):
+def test_animals_dont_migrate(mocker,create_c, create_s):
     mocker.patch('numpy.random.random', return_value=1)
-    c.animals_migrate()
-    assert len(s.object_matrix[1][1].animal_object_list) == 140
-    assert len(s.object_matrix[1][2].animal_object_list) == 0
+    create_c.animals_migrate()
+    assert len(create_s.object_matrix[1][1].animal_object_list) == 140
+    assert len(create_s.object_matrix[1][2].animal_object_list) == 0
 
-def test_animals_migrate():
-    c.animals_migrate()
-    assert len(s.object_matrix[1][1].animal_object_list) > 0
-    assert len(s.object_matrix[1][1].animal_object_list) < 140
-    assert len(s.object_matrix[1][2].animal_object_list) > 0
-    assert len(s.object_matrix[1][2].animal_object_list) < 140
+def test_animals_migrate(create_c, create_s):
+    create_c.animals_migrate()
+    assert len(create_s.object_matrix[1][1].animal_object_list) > 0
+    assert len(create_s.object_matrix[1][1].animal_object_list) < 140
+    assert len(create_s.object_matrix[1][2].animal_object_list) > 0
+    assert len(create_s.object_matrix[1][2].animal_object_list) < 140
 
-def test_all_animals_die(mocker):
+def test_all_animals_die(mocker, create_c, create_s ):
     mocker.patch('numpy.random.random', return_value=0)
-    c.animals_die()
-    assert s.object_matrix[1][1].animal_object_list == []
+    create_c.animals_die()
+    assert create_s.object_matrix[1][1].animal_object_list == []
 
-def test_no_animals_die(mocker):
+def test_no_animals_die(mocker, create_c, create_s):
     mocker.patch('numpy.random.random', return_value=1)
-    old_list = s.object_matrix[1][1].animal_object_list
-    c.animals_die()
-    assert s.object_matrix[1][1].animal_object_list == old_list
+    old_list = create_s.object_matrix[1][1].animal_object_list
+    create_c.animals_die()
+    assert create_s.object_matrix[1][1].animal_object_list == old_list
 
-def test_animals_die(mocker):
-    mocker.patch('numpy.random.random', return_value=0.001)
-    old_len = len(s.object_matrix[1][1].animal_object_list)
-    c.animals_die()
-    assert len(s.object_matrix[1][1].animal_object_list) < old_len
 
-def test_death_probability(mocker):
+def test_death_probability(mocker, create_c, create_s):
     mocker.patch('numpy.random.random', return_value=0.001)
-    obj_id = id( s.object_matrix[1][1].animal_object_list[0] )
-    death_prob = s.object_matrix[1][1].animal_object_list[0].death_prob
-    c.animals_die()
-    list_of_obj_id = [id(a) for a in s.object_matrix[1][1].animal_object_list]
+    obj_id = id( create_s.object_matrix[1][1].animal_object_list[0] )
+    death_prob = create_s.object_matrix[1][1].animal_object_list[0].death_prob
+    create_c.animals_die()
+    list_of_obj_id = [id(a) for a in create_s.object_matrix[1][1].animal_object_list]
     if numpy.random.random() < death_prob:
         assert obj_id not in list_of_obj_id
     else:
