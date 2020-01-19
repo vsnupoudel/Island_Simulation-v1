df844662bc73f17bcd9beb63763abf7370459816
vsnupoudel <replytobishnu@gmail.com>
2020-01-19 21:43:07 +0100

    Modified fixtures to make tests shorter

--- tests/test_Cycle.py
+++ tests/test_Cycle.py
@@ -13,6 +13,7 @@ from pytest_mock import mocker
 import pytest
 import numpy
 
+
 @pytest.fixture()
 def input_map():
     map = ("""\
@@ -20,6 +21,7 @@ def input_map():
                     OJSO
                     OOOO""")
     return map
+
 
 @pytest.fixture()
 def ini_herbs():
@@ -42,15 +44,18 @@ def ini_herbs():
                 ]
     return herbs
 
+
 @pytest.fixture()
 def create_s(input_map,ini_herbs):
     s = BioSim(input_map, ini_herbs, seed=1)
     return s
 
+
 @pytest.fixture()
 def create_c(input_map,ini_herbs, create_s):
     c = Cycle(create_s.object_matrix)
     return c
+
 
 def test_get_adjacent_migratable_cells(create_c):
     """should only get migratable cells"""
@@ -58,11 +63,13 @@ def test_get_adjacent_migratable_cells(create_c):
                        create_c.get_adjacent_migratable_cells(1, 1)]
     assert migratable_cells == ['Savannah']
 
+
 def test_food_grows_Savannah(create_c):
     """Food amount in each Savannah cell should increase"""
     prev_food_sav = create_c.object_matrix[1][2].f_ij
     create_c.food_grows()
     assert create_c.object_matrix[1][2].f_ij > prev_food_sav
+
 
 def test_max_food_Jungle(create_c):
     """When food grows in Jungle cells it should be set to f_max = 800"""
@@ -72,6 +79,7 @@ def test_max_food_Jungle(create_c):
     assert create_c.object_matrix[1][1].f_ij >= prev_food_jun
     assert create_c.object_matrix[1][1].f_ij == create_c.object_matrix[1][1].parameters[
         'f_max']
+
 
 def test_fitness_increase_after_feeding(create_c):
     prev_fitness_array = [ a.fitness for a in create_c.object_matrix[1][
@@ -98,17 +106,20 @@ def test_animals_reproduce(input_map, ini_herbs):
     assert carn_count_curr > carn_count_prev
     assert herb_count_curr > herb_count_prev
 
+
 def test_animals_migrate(mocker, create_c, create_s):
     mocker.patch('numpy.random.random', return_value=0)
     create_c.animals_migrate()
     assert len(create_s.object_matrix[1][1].animal_object_list) == 0
     assert len(create_s.object_matrix[1][2].animal_object_list) == 140
 
+
 def test_animals_dont_migrate(mocker,create_c, create_s):
     mocker.patch('numpy.random.random', return_value=1)
     create_c.animals_migrate()
     assert len(create_s.object_matrix[1][1].animal_object_list) == 140
     assert len(create_s.object_matrix[1][2].animal_object_list) == 0
+
 
 def test_animals_migrate(create_c, create_s):
     create_c.animals_migrate()
@@ -117,10 +128,12 @@ def test_animals_migrate(create_c, create_s):
     assert len(create_s.object_matrix[1][2].animal_object_list) > 0
     assert len(create_s.object_matrix[1][2].animal_object_list) < 140
 
+
 def test_all_animals_die(mocker, create_c, create_s ):
     mocker.patch('numpy.random.random', return_value=0)
     create_c.animals_die()
     assert create_s.object_matrix[1][1].animal_object_list == []
+
 
 def test_no_animals_die(mocker, create_c, create_s):
     mocker.patch('numpy.random.random', return_value=1)
@@ -134,8 +147,8 @@ def test_death_probability(mocker, create_c, create_s):
     obj_id = id( create_s.object_matrix[1][1].animal_object_list[0] )
     death_prob = create_s.object_matrix[1][1].animal_object_list[0].death_prob
     create_c.animals_die()
-    list_of_obj_id = [id(a) for a in create_s.object_matrix[1][1].animal_object_list]
+    list_id = [id(a) for a in create_s.object_matrix[1][1].animal_object_list]
     if numpy.random.random() < death_prob:
-        assert obj_id not in list_of_obj_id
+        assert obj_id not in list_id
     else:
-        assert obj_id in list_of_obj_id
+        assert obj_id in list_id
