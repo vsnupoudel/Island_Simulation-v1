8e885a35c5639eb11585efaf750a8b9b5b6b2006
vsnupoudel <replytobishnu@gmail.com>
2020-01-20 13:38:13 +0100

    tests for simulation and visualization

--- tests/test_simulation.py
+++ tests/test_simulation.py
@@ -1,9 +1,5 @@
 # -*- coding: utf-8 -*-
 
-"""
-Tests for Animal class
-"""
-
 __author__ = "Anders Huse, Bishnu Poudel"
 __email__ = "anhuse@nmbu.no; bipo@nmbu.no"
 
@@ -14,257 +10,118 @@ import numpy as np
 import pytest
 from pytest_mock import mocker
 
-@pytest.fixture()
-def input_map():
-    """returns map where required in tests below"""
-    map = """\
-                 OOOOOOOOO
-                 OSSJJSSOO
-                 OOOOOOOOO
-                 """
-    return map
+class TestSimulation:
+    """Tests for the Biosim class"""
 
-@pytest.fixture()
-def ini_pop():
-    """returns herbivore population where required in tests below"""
-    herbs = [
-        {
-            "loc": (2, 2),
-            "pop": [
-                {"species": "Herbivore", "age": 5, "weight": 20}
-                for _ in range(200)
-            ]+[
-                {"species": "Carnivore", "age": 5, "weight": 20}
-                for _ in range(200)
-            ]
-        },
-        {
-            "loc": (2, 3),
-            "pop": [
-                {"species": "Herbivore", "age": 5, "weight": 20}
-                for _ in range(200)
-            ]
-        }
-    ]
-    return herbs
+    @pytest.fixture()
+    def input_map():
+        """returns map where required in tests below"""
+        map = """\
+                     OOOOOOOOO
+                     OSSJJSSOO
+                     OOOOOOOOO
+                     """
+        return map
 
-@pytest.fixture()
-def add_carns():
-    """
-    Returns carnivore population where required in tests below
-    Population should be an iterable
-    """
-    carns = [
-        {
-            "loc": (2, 2),
-            "pop": [
-                {"species": "Carnivore", "age": 5, "weight": 20}
-                for _ in range(40)
-            ],
-        }
-    ]
-    return carns
+    @pytest.fixture()
+    def ini_pop():
+        """returns herbivore population where required in tests below"""
+        herbs = [
+            {
+                "loc": (2, 2),
+                "pop": [
+                    {"species": "Herbivore", "age": 5, "weight": 20}
+                    for _ in range(200)
+                ]+[
+                    {"species": "Carnivore", "age": 5, "weight": 20}
+                    for _ in range(200)
+                ]
+            },
+            {
+                "loc": (2, 3),
+                "pop": [
+                    {"species": "Herbivore", "age": 5, "weight": 20}
+                    for _ in range(200)
+                ]
+            }
+        ]
+        return herbs
 
-@pytest.fixture()
-def create_s(input_map, ini_pop):
-    s = BioSim(input_map, ini_pop, seed=1)
-    return s
+    @pytest.fixture()
+    def add_carns():
+        """
+        Returns carnivore population where required in tests below
+        Population should be an iterable
+        """
+        carns = [
+            {
+                "loc": (2, 2),
+                "pop": [
+                    {"species": "Carnivore", "age": 5, "weight": 20}
+                    for _ in range(40)
+                ],
+            }
+        ]
+        return carns
 
-
-def test_set_landscape_parameters(create_s):
-    "set_landscape_parameters shold change parameters sucsessfully"
-    create_s.set_landscape_parameters("S", {"f_max": 700})
-    create_s.set_landscape_parameters("J", {"f_max": 600})
-    assert Savannah.parameters["f_max"] == 700
-    assert Jungle.parameters["f_max"] == 600
+    @pytest.fixture()
+    def create_s(input_map, ini_pop):
+        s = BioSim(input_map, ini_pop, seed=1)
+        return s
 
 
-def test_set_animal_parameters(create_s):
-    "set_animal_parameters shold change parameters sucsessfully"
-
-    create_s.set_animal_parameters("Herbivore", {"zeta": 3.2, "xi": 1.8})
-    create_s.set_animal_parameters("Carnivore", {"zeta": 5.0, "xi": 2.0})
-
-    assert Herbivore.p["zeta"] == 3.2
-    assert Herbivore.p["xi"] == 1.8
-    assert Carnivore.p["zeta"] == 5.0
-    assert Carnivore.p["xi"] == 2.0
+    def test_set_landscape_parameters(self, create_s):
+        "set_landscape_parameters shold change parameters sucsessfully"
+        create_s.set_landscape_parameters("S", {"f_max": 700})
+        create_s.set_landscape_parameters("J", {"f_max": 600})
+        assert Savannah.parameters["f_max"] == 700
+        assert Jungle.parameters["f_max"] == 600
 
 
-def test_num_animals_and_add_population(input_map, ini_pop, add_carns):
-    """"""
-    s = BioSim(input_map, ini_pop, seed=1)
-    print('')
-    print(s.num_animals['Herbivore'])
-    print(s.num_animals['Carnivore'])
-    assert s.num_animals['Herbivore'] > 0
-    assert s.num_animals['Carnivore'] > 0
-    prev_carns = s.num_animals['Carnivore']
-    s.add_population(add_carns)
-    assert s.num_animals['Carnivore'] > prev_carns
+    def test_set_animal_parameters(create_s):
+        "set_animal_parameters shold change parameters sucsessfully"
+
+        create_s.set_animal_parameters("Herbivore", {"zeta": 3.2, "xi": 1.8})
+        create_s.set_animal_parameters("Carnivore", {"zeta": 5.0, "xi": 2.0})
+
+        assert Herbivore.p["zeta"] == 3.2
+        assert Herbivore.p["xi"] == 1.8
+        assert Carnivore.p["zeta"] == 5.0
+        assert Carnivore.p["xi"] == 2.0
 
 
-def test_shape_herbivore_distributin(create_s):
-    """shape of herbivore_distribution should be the same as for
-    the object_matrix"""
-    assert np.shape(create_s.herbivore_distribution) ==\
-    np.shape(create_s.object_matrix)
+    def test_num_animals_and_add_population(input_map, ini_pop, add_carns):
+        """"""
+        s = BioSim(input_map, ini_pop, seed=1)
+        print('')
+        print(s.num_animals['Herbivore'])
+        print(s.num_animals['Carnivore'])
+        assert s.num_animals['Herbivore'] > 0
+        assert s.num_animals['Carnivore'] > 0
+        prev_carns = s.num_animals['Carnivore']
+        s.add_population(add_carns)
+        assert s.num_animals['Carnivore'] > prev_carns
 
 
-def test_shape_carnivore_distributin(create_s):
-    """shape of carnivore_distribution should be the same as for
-     the object_matrix"""
-    assert np.shape(create_s.carnivore_distribution) == \
-    np.shape(create_s.object_matrix)
-
-def test_carnivore_distributin(create_s):
-    """test for carnivore_distribution property"""
-    print('')
-    assert 200 in create_s.carnivore_distribution
+    def test_shape_herbivore_distributin(create_s):
+        """shape of herbivore_distribution should be the same as for
+        the object_matrix"""
+        assert np.shape(create_s.herbivore_distribution) ==\
+        np.shape(create_s.object_matrix)
 
 
-# def test_island_matrix_shape():
-#     """test island_matrix property"""
-#     assert np.shape(s.island_matrix) == np.shape(s.object_matrix)
-#
-#
-# def test_island_matrix():
-#     """test island_matrix property"""
-#     pass
-#
-#
-# def test_simulation():
-#     """test for simulation method"""
-#     pass
+    def test_shape_carnivore_distributin(create_s):
+        """shape of carnivore_distribution should be the same as for
+         the object_matrix"""
+        assert np.shape(create_s.carnivore_distribution) == \
+        np.shape(create_s.object_matrix)
 
-# Put all tests in a class maybe
+    def test_carnivore_distributin(create_s):
+        """test for carnivore_distribution property"""
+        assert 200 in create_s.carnivore_distribution
 
-# class TestSimulation:
-#     """
-#     Several tests for the Simulation class
-#     """
-#
-#     @pytest.fixture()
-#     def input_map(self):
-#         """returns map where required in tests below"""
-#         map = """\
-#                      OOOOOOOOO
-#                      OSSJJSSOO
-#                      OOOOOOOOO
-#                      """
-#         return map
-#
-#     @pytest.fixture()
-#     def ini_herbs(self):
-#         """returns herbivore population where required in tests below"""
-#         herbs = [
-#             {
-#                 "loc": (2, 2),
-#                 "pop": [
-#                            {"species": "Herbivore", "age": 5, "weight": 20}
-#                            for _ in range(200)
-#                        ] + [
-#                            {"species": "Carnivore", "age": 5, "weight": 20}
-#                            for _ in range(200)
-#                        ]
-#             },
-#             {
-#                 "loc": (2, 3),
-#                 "pop": [
-#                     {"species": "Herbivore", "age": 5, "weight": 20}
-#                     for _ in range(200)
-#                 ]
-#             }
-#         ]
-#         return herbs
-#
-#     @pytest.fixture()
-#     def ini_carns(self):
-#         carns = [
-#             {
-#                 "loc": (2, 2),
-#                 "pop": [
-#                     {"species": "Carnivore", "age": 5, "weight": 20}
-#                     for _ in range(40)
-#                 ]
-#             }
-#         ]
-#         return carns
-#
-#     @pytest.fixture()
-#     def create_s(self,input_map, ini_herbs):
-#         s = BioSim(input_map, ini_herbs, seed=1)
-#         return s
-#
-#     def test_set_landscape_parameters(self, create_s):
-#         "set_landscape_parameters shold change parameters sucsessfully"
-#
-#         create_s.set_landscape_parameters("S", {"f_max": 700})
-#         create_s.set_landscape_parameters("J", {"f_max": 600})
-#
-#         assert Savannah.parameters["f_max"] == 700
-#         assert Jungle.parameters["f_max"] == 600
-#
-#     def test_set_animal_parameters(self, create_s):
-#         "set_animal_parameters shold change parameters sucsessfully"
-#
-#         create_s.set_animal_parameters("Herbivore", {"zeta": 3.2, "xi": 1.8})
-#         create_s.set_animal_parameters("Carnivore", {"zeta": 5.0, "xi": 2.0})
-#
-#         assert Herbivore.p["zeta"] == 3.2
-#         assert Herbivore.p["xi"] == 1.8
-#         assert Carnivore.p["zeta"] == 5.0
-#         assert Carnivore.p["xi"] == 2.0
-#
-#     def test_num_animals(self, input_map, ini_herbs):
-#         """"""
-#         s = BioSim(input_map, ini_carns, seed=1)
-#         print('')
-#         print(s.num_animals['Herbivore'])
-#         print(s.num_animals['Carnivore'])
-#         assert s.num_animals['Herbivore'] > 0
-#         # assert s.num_animals['Carnivore'] > 0
-#         # s.add_population(ini_carns)
-#         # assert create_s.num_animals['Carnivore'] > 0
-#
-#     def test_add_population(self):
-#         """"""
-#         # pop before
-#         s.add_population(ini_carns)
-#         # pop after
-#         # assert pop_after > pop_before
-#         pass
-#
-#     def test_shape_herbivore_distributin(self):
-#         """shape of herbivore_distribution should be the same as for
-#         the object_matrix"""
-#         np.shape(s.herbivore_distribution.shape) == np.shape(s.object_matrix)
-#         pass
-#
-#     def test_shape_carnivore_distributin(self):
-#         """shape of carnivore_distribution should be the same as for
-#          the object_matrix"""
-#         np.shape(s.carnivore_distribution.shape) == np.shape(s.object_matrix)
-#         pass
-#
-#     def test_carnivore_distributin(self):
-#         """test for carnivore_distribution property"""
-#         s.carnivore_distribution
-#         pass
-#
-#     def test_animal_distributin(self):
-#         """test for animal_distribution property"""
-#         s.animal_distribution
-#         pass
-#
-#     def test_island_matrix_shape(self):
-#         """test island_matrix property"""
-#         assert np.shape(s.island_matrix) == np.shape(s.object_matrix)
-#
-#     def test_island_matrix(self):
-#         """test island_matrix property"""
-#         pass
-#
-#     def test_simulation(self):
-#         """test for simulation method"""
-#         pass
+
+    def test_island_matrix_shape(create_s):
+        """test island_matrix property"""
+        assert np.shape(create_s.island_matrix) == np.shape(create_s.object_matrix)
+
