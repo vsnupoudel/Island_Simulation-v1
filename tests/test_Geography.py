61eaef7769f7d0b5e9b5da094ec8fb1f9c23f831
Anders Huse <anhuse@nmbu.no>
2020-01-21 09:43:07 +0100

    Rewrote the TestGeo class

--- tests/test_Geography.py
+++ tests/test_Geography.py
@@ -11,40 +11,41 @@ from src.biosim.Geography import Geo
 import pytest
 
 
-def test_proper_letters():
-    """all letters should either be: = O, M, J, S, D"""
-    with pytest.raises(ValueError):
-        Geo("yyyy")
+class TestGeo:
+    """
+    Several tests for the Geo class
+    """
+    @pytest.fixture()
+    def create_map(self):
+        """Creates a Geo object"""
+        return Geo("""\
+            OOO
+            OJO
+            OOO""")
 
+    def test_map_gives_object_output(self, create_map):
 
-def test_same_legth():
-    """rows have same length"""
-    with pytest.raises(ValueError):
-        Geo(""""\
-                OOOOOOO
-                JJJJJ""")
+        empty_list = []
+        for row in create_map.object_matrix:
+            empty_list.append([type(obj).__name__ for obj in row])
+        assert empty_list == [['Ocean', 'Ocean', 'Ocean'],
+                              ['Ocean', 'Jungle', 'Ocean'],
+                              ['Ocean', 'Ocean', 'Ocean']]
 
+    def test_proper_letters(self):
+        """all letters should either be: = O, M, J, S, D"""
+        with pytest.raises(ValueError):
+            Geo("yyyy")
 
-def test_ocean_edges():
-    with pytest.raises(ValueError):
-        Geo(""""\
-                OOOOO
-                JJJJJ""")
+    def test_same_legth(self):
+        """rows have same length"""
+        with pytest.raises(ValueError):
+            Geo(""""\
+                    OOOOOOO
+                    JJJJJ""")
 
-
-def test_map_gives_object_output():
-    g = Geo("""\
-    OOO
-    OJO
-    OOO""")
-    empty_list = []
-    for row in g.object_matrix:
-        empty_list.append([type(obj).__name__ for obj in row])
-    assert empty_list == [['Ocean', 'Ocean', 'Ocean'],
-                          ['Ocean', 'Jungle', 'Ocean'],
-                          ['Ocean', 'Ocean', 'Ocean']]
-
-
-
-
-
+    def test_ocean_edges(self):
+        with pytest.raises(ValueError):
+            Geo(""""\
+                    OOOOO
+                    JJJJJ""")
