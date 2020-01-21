3902b159f102862dc25a70bacda4a2387f613977
vsnupoudel <replytobishnu@gmail.com>
2020-01-21 09:40:48 +0100

    Edited test_Visualization.py

--- tests/test_Visualization.py
+++ tests/test_Visualization.py
@@ -44,11 +44,11 @@ class TestVisualization:
     @pytest.fixture()
     def create_vis(self, create_sim):
         """Creates a Visualization object"""
-        return Visualization(create_sim.object_matrix)
+        return Visualization()
 
     def test_set_graphics(self, create_vis):
         """tests subplots of set_graphics"""
-        create_vis._set_graphics(5)
+        create_vis.set_graphics(100, 20)
         assert create_vis._fig is not None, "Figure should not be None"
         assert create_vis._map_ax is not None, "Figure should not be None"
         assert create_vis._herb_line is not None, "Figure should not be None"
@@ -58,15 +58,15 @@ class TestVisualization:
 
     def test_create_map(self, create_vis, create_sim):
         """Map should be created"""
-        create_vis._set_graphics(5)
+        create_vis.set_graphics(100, 20)
         create_vis.create_map(create_sim.island_matrix)
         assert create_vis._img_axis is not None, "Figure should not be None"
 
     def test_update_herb_ax(self, create_vis, create_sim):
         """map of herbivores should be created"""
-        create_vis._set_graphics(5)
+        create_vis.set_graphics(100, 20)
         ax_1 = create_vis._herb_axis
-        create_vis.update_herb_ax(create_sim.herbivore_distribution)
+        create_vis.update_herb_ax(create_sim.herbivore_distribution, 20)
         ax_2 = create_vis._herb_axis
 
         assert create_vis._herb_axis is not None, "Figure should not be None"
@@ -74,9 +74,9 @@ class TestVisualization:
 
     def test_update_carn_ax(self, create_vis, create_sim):
         """map of carnivores should be created"""
-        create_vis._set_graphics(5)
+        create_vis.set_graphics(100, 20)
         ax_1 = create_vis._carn_axis
-        create_vis.update_carn_ax(create_sim.carnivore_distribution)
+        create_vis.update_carn_ax(create_sim.carnivore_distribution, 20)
         ax_2 = create_vis._carn_axis
 
         assert create_vis._carn_axis is not None, "Figure should not be None"
