22408ee15b45cba8c328d37ba71e6e1a79291dbf
vsnupoudel <replytobishnu@gmail.com>
2020-01-22 11:39:06 +0100

    Commit for submission

--- src/biosim/geography.py
+++ src/biosim/geography.py
@@ -9,7 +9,7 @@ __email__ = "anhuse@nmbu.no; bipo@nmbu.no"
 
 import numpy as np
 import textwrap
-from terrain import Jungle, Savannah, Desert, Ocean, Mountain
+from .terrain import Jungle, Savannah, Desert, Ocean, Mountain
 
 
 class CreateMap:
--- src/biosim/simulation.py
+++ src/biosim/simulation.py
@@ -4,11 +4,11 @@
 __author__ = "Anders Huse"
 __email__ = "huse.anders@gmail.com"
 
-from cycle import Cycle
-from geography import CreateMap
-from visualization import Visualization
-from terrain import Savannah, Jungle
-from animal import Herbivore, Carnivore
+from .cycle import Cycle
+from .geography import CreateMap
+from .visualization import Visualization
+from .terrain import Savannah, Jungle
+from .animal import Herbivore, Carnivore
 
 import numpy as np
 import pandas as pd
